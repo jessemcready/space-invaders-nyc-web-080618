3ac14525874988ed1eb9d63998419d06e944f25f
Jesse McReady <jessemcready@gmail.com>
2018-09-19 14:28:17 -0400

    Done.

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,19 @@
-class Spaceship {}
+class Spaceship {
+  constructor(vessel, crew, phasers, shields) {
+    this.name = vessel
+    this.crew = crew
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = false
+    this.warpDrive = "disengaged"
+    this.phasersCharge = "uncharged"
+    if (this.crew.length > 0) {
+      this.docked = false
+      for (let elem of this.crew) {
+        elem.currentShip = this
+      }
+    } else {
+      this.docked = true
+    }
+  }
+}
